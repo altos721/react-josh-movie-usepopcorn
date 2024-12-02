a5436574098877a38d34877ddb027f37dc345f6f
altos721 <altos721@gmail.com>
2024-12-02 21:57:12 +0800

    Modify star-rating with props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color='red'
+      className='test'
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
